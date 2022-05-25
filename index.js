a3a997fb8e9ed57cb04f4f64496c49554e3f50cf
severlas <sergeygoncharov9@gmail.com>
2022-05-25 11:43:22 +0300

    edited index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 window.addEventListener('load', function () {
   const fac = new FastAverageColor(),
-        gradient = document.querySelector('.color-card__gradient'),
-        color = fac.getColor(document.querySelector('img'));
+        containerImg = document.querySelector('.color-card__img'),
+        gradient = document.querySelector('.color-card__img ~ .color-card__gradient'),
+
+        color = fac.getColor(containerImg.querySelector('img'));
 
   gradient.style.background = `linear-gradient(rgba(0,0,0,0.44), ${color.rgb} 100%`;
 });
